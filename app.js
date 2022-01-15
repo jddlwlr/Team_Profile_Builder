1b9c9e32621224c899bc11d6ec3701b5118f4f4b
Jesse Lawler <jedd.lawler@gmail.com>
2022-01-14 21:19:17 -0700

    updated classes and prompts

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//Requiring dependancies
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+//Requiring classes
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager")
-
+const Intern = require("./lib/Intern")
 var team = [];
 
+//Manager prompt
 const buildManager = () => {
  return inquirer.prompt ([
     {
@@ -37,38 +41,53 @@ const buildManager = () => {
     console.log(manager);
   })
 }; 
-
+//Employee prompt
 const buildEmployee = () => {
   return inquirer.prompt ([
     {
       type: 'list',
       name:'role',
-      message: 'Select a role:'
+      message: 'Select a role:',
       choices: ['Engineer', 'Intern']
     },
     {
       type: 'input',
       name: 'id',
-      message: 'Manager ID number:'
+      message: 'Employee ID #:'
     },
     {
       type: 'input',
       name: 'email',
-      message: 'Manager email address:'
-    }
+      message: 'Employee email address:'
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Interns School:',
+      when: (input) => input.role === "Intern"
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Engineers GitHub useranme:',
+      when: (input) => input.role === "Engineer"
+    },
   ])
   .then(employeeInput => {
-    let {name, id, email, role, github, school} = employeeInput
+    let {name, id, email, role, github, school,} = employeeInput
     let employee;
 
     if (role === "Engineer"){
       employee = new Engineer (name, id, email, github)
     } else if (role === "Intern"){
       employee = new Intern (name, id, email, school)
-      
     }
-  }
-}
+    team.push(Employee);
+  })
+};
+console.log(Employee);
 
 buildManager()
 .then(buildEmployee)
+
+// const writeFile = fs.writeFile('./dist', index.html')
